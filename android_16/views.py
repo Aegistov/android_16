45d8d17a7f69694a30544bb07fa3bc7debb6a34c
Michael J <mmorel@michaels-mbp.home>
2019-12-24 00:44:40 -0500

    boiler stuff

---
+++ android_16/views.py
@@ -0,0 +1,5 @@
+from android_16 import app
+
+@app.route('/')
+def index():
+    return 'Hello World!'
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='android_16',
+    packages=['android_16'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+)
